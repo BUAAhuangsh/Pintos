855e6f2c1a5620b1c1902efabd0949d56cd0f8d6
wellmslouis <70185508+wellmslouis@users.noreply.github.com>
2021-11-30 13:03:07 +0800

    sys初步

--- Project2/src/userprog/syscall.c
+++ Project2/src/userprog/syscall.c
@@ -10,11 +10,104 @@ void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  syscalls[SYS_HALT] = &sys_halt;
+  syscalls[SYS_EXIT] = &sys_exit;
+  syscalls[SYS_EXEC] = &sys_exec;
+  syscalls[SYS_WAIT] = &sys_wait;
+  syscalls[SYS_CREATE] = &sys_create;
+  syscalls[SYS_REMOVE] = &sys_remove;
+  syscalls[SYS_OPEN] = &sys_open;
+  syscalls[SYS_WRITE] = &sys_write;
+  syscalls[SYS_SEEK] = &sys_seek;
+  syscalls[SYS_TELL] = &sys_tell;
+  syscalls[SYS_CLOSE] =&sys_close;
+  syscalls[SYS_READ] = &sys_read;
+  syscalls[SYS_FILESIZE] = &sys_filesize;
+}
+
+static int 
+get_user (const uint8_t *uaddr)
+{
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:" : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+
+void * 
+check_ptr2(const void *vaddr)
+{ 
+  /* Judge address */
+  if (!is_user_vaddr(vaddr))
+  {
+    exit_special ();
+  }
+  /* Judge the page */
+  void *ptr = pagedir_get_page (thread_current()->pagedir, vaddr);
+  if (!ptr)
+  {
+    exit_special ();
+  }
+  /* Judge the content of page */
+  uint8_t *check_byteptr = (uint8_t *) vaddr;
+  for (uint8_t i = 0; i < 4; i++) 
+  {
+    if (get_user(check_byteptr + i) == -1)
+    {
+      exit_special ();
+    }
+  }
+
+  return ptr;
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
+  int * p = f->esp;
+  check_ptr2 (p + 1);//检查有效性
+  int type = * (int *)f->esp;//记录在栈顶的系统调用类型type
+  if(type <= 0 || type >= max_syscall){
+    exit_special ();//类型错误，退出
+  }
+  syscalls[type](f);//类型正确，查找数组调用对应系统调用并调用执行
+}
+
+void 
+sys_halt (struct intr_frame* f)
+{
+  shutdown_power_off();
+}
+
+/* Do sytem exit */
+void 
+sys_exit (struct intr_frame* f)
+{
+  uint32_t *user_ptr = f->esp;
+  check_ptr2 (user_ptr + 1);
+  *user_ptr++;
+  /* record the exit status of the process */
+  thread_current()->st_exit = *user_ptr;
   thread_exit ();
 }
+
+/* Do sytem exec */
+void 
+sys_exec (struct intr_frame* f)
+{
+  uint32_t *user_ptr = f->esp;
+  check_ptr2 (user_ptr + 1);
+  check_ptr2 (*(user_ptr + 1));
+  *user_ptr++;
+  f->eax = process_execute((char*)* user_ptr);
+}
+
+/* Do sytem wait */
+void 
+sys_wait (struct intr_frame* f)
+{
+  uint32_t *user_ptr = f->esp;
+  check_ptr2 (user_ptr + 1);
+  *user_ptr++;
+  f->eax = process_wait(*user_ptr);
+}
+
